173cff5ea61320c8e8a7c7acb8054dd5d8ae24cd
Pulkit2121 <rajpalpulkit21@gmail.com>
2018-04-07 12:45:45 +0530

    adding files

---
+++ public/index.js
@@ -0,0 +1,67 @@
+ var socket;
+//
+// // console.log(socket.id);
+//
+// $(function () {
+//
+//     socket=io.connect('http://localhost:4000');
+//     let loginContainer=$('#login-container');
+//     let chatContainer=$('#chat-container');
+//
+//     chatContainer.hide()
+//
+//     let username=$('#username');
+//     let login=$('#login');
+//     let message = $('#message');
+//     let send = $('#send');
+//     let list = $('#list');
+//
+//     login.click(function () {
+//         socket.emit('login', { username: username.val()})
+//     })
+//
+//     socket.on('logged-in', (data)=>{
+//         chatContainer.show();
+//         loginContainer.hide();
+//     })
+//
+//     send.click(function () {
+//         socket.emit('send_message', {message:message.val()}
+//         )
+//     })
+//
+//     socket.on('receive_message', (data)=>{
+//         list.append(`<li> ${data.username} : ${data.message}: ${data.timestamp} </li>`)
+//     })
+// })
+$(function () {
+    socket=io.connect('http://localhost:4000');
+    let loginContainer = $('#login-container');
+    let chatContainer = $('#chat-container');
+
+    chatContainer.hide();
+
+    let username = $('#username');
+    let login = $('#login');
+    let message = $('#message');
+    let send = $('#send');
+    let list = $('#list');
+
+    login.click(function () {
+        socket.emit('login', {username: username.val()})
+    })
+
+    socket.on('logged-in', (data) => {
+        chatContainer.show();
+        loginContainer.hide();
+    })
+
+    send.click(function () {
+        socket.emit('send_message', {message: message.val()})
+    })
+
+    socket.on('receive_message', (data) => {
+
+        list.append(`<li>${data.username} : ${data.message} : ${data.timestamp}</li>`)
+    })
+})
